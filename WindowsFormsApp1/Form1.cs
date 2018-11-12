30b6febbfb5285a60a42fad458c428d09b1e8c2b
Han <lhu3319@nate.com>
2018-11-12 10:59:29 +0900

    button 함수 생성

---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            Load += Form1_Load;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Button btn = new Button();
+            btn.DialogResult = DialogResult.OK;
+            btn.Text = "확인";
+            btn.Size = new Size(100, 50);
+            btn.Location = new Point(30, 30);
+            Controls.Add(btn);
+            btn.Click += btn_click;
+        }
+
+        private void btn_click(object o, EventArgs a)
+        {
+            MessageBox.Show("확인","확인창");
+        }
+    }
+}
